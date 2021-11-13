36a0e66457003550e3d339dad9850e1a6b175d5d
DimaShumanskiy <dimaihfn@gmail.com>
2021-11-13 13:32:00 +0300

    Use Memo

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -16,7 +16,7 @@ const UsersSecret = (props: { users: Array<string> }) => {
 
 const Users = React.memo(UsersSecret)
 
-export const Example1 = () => {
+export const Ex1 = () => {
     const [counter, setCounter] = useState(0);
     const [users, setUsers] = useState(
         ['dima', 'valera', 'artem']
@@ -29,8 +29,7 @@ export const Example1 = () => {
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
-        <button onClick={addUser}>add user
-        </button>
+        <button onClick={addUser}>add user</button>
         <NewMessagesCounter count={counter}/>
         <Users users={users}/>
     </>
